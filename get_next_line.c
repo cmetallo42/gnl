7e5f0ab28f85e97bd16f6c2e3c5546da73da8082
cmetallo <denerohin@yandex.ru>
2020-07-28 20:45:50 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cmetallo <denerohin@yandex.ru>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/27 18:58:37 by cmetallo          #+#    #+#             */
-/*   Updated: 2020/07/27 19:11:26 by cmetallo         ###   ########.fr       */
+/*   Updated: 2020/07/28 20:36:06 by cmetallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,64 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "/Users/cmetallo/Desktop/libft/libft.h"
+
+char	*check_remaining(char *remaining, char **line)
+{
+	char	*ptn;
+
+	ptn = NULL;
+	if (remaining)
+	{
+		if ((ptn = ft_strchr(remaining, '\n')))
+		{
+			*ptn = '\0';
+			*line = ft_strdup(remaining);
+			ft_strcpy(remaining, ++ptn);
+		}
+		else
+		{
+			*line = ft_strdup(remaining);
+			ft_strclr(remaining);
+		}
+	}
+	else
+		*line = "\0";
+	return (ptn);
+}
 
 int	get_next_line(int fd, char **line)
 {
+	char	buf[10 + 1];
+	int		bwr;
+	char	*ptn;
+	static char *remaining;
+
+	ptn = check_remaining(remaining, line);
+	while (!ptn && (bwr = read(fd, buf, 10)))
+	{
+		if ((ptn = ft_strchr(buf, '\n')))
+		{
+			*ptn = '\0';
+			ptn++;
+			remaining = ft_strdup(ptn);
+		}
+				
+		buf[bwr] = '\0';
+		*line = ft_strjoin(*line, buf);
+	}
 	return (0);
 }
 
-int main(void)
+int	main(void)
 {
-	char *line;
-	int fd;
+	char	*line;
+	int		fd;
 
 	fd = open("text.txt", O_RDONLY);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+    printf("%s\n", line);
+	return (0);
 }
